e945b413dfe25e762c4c36911ea0d8c9532b0c6d
OusManDiouf <java-89@hotmail.fr>
2022-10-23 20:09:31 +0000

    done

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type account struct {
+	balance float32
+}
+
 func main() {
-	fmt.Println("Go!!!!!")
+	accounts := []account{
+		{balance: 100.},
+		{balance: 200.},
+		{balance: 300.},
+	}
+
+	for i := range accounts {
+		accounts[i].balance += 1000
+	}
+
+	fmt.Println(accounts)
+
 }
