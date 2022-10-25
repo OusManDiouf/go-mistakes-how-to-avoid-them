a4138bafb7e8543282fb476029f634b6d9380f8f
OusManDiouf <java-89@hotmail.fr>
2022-10-25 15:17:54 +0000

    done

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type ElementsSlice []string
+
+func (e *ElementsSlice) add(str string) {
+	*e = append(*e, str)
+}
+
+type ElementsMap map[int]string
+
+func (m ElementsMap) add(k int, v string) {
+	m[k] = v
+}
+
 func main() {
-	fmt.Println("Go!!!!!")
+	elementsSlice := ElementsSlice{"Vanuatu", "Montenegro"}
+	elementsSlice.add("Ethiopia")
+	//fmt.Println(elements)
+
+	elementsMap := ElementsMap{}
+	elementsMap.add(1, "Germany")
+	fmt.Println(elementsMap)
 }
