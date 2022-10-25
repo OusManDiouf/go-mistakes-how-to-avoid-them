c63b540712b973c57e08829875c4d06f4e58a662
OusManDiouf <java-89@hotmail.fr>
2022-10-25 23:48:13 +0000

    done

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+const (
+	StatusSuccess  = "success"
+	StatusErrorFoo = "error_foo"
+	StatusErrorBar = "error_bar"
+)
 
 func main() {
-	fmt.Println("Go!!!!!")
+
+	logger := LogCounterMap{}
+	for i := 0; i < 5; i++ {
+		rand.Seed(time.Now().UnixNano())
+		err := f(logger)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	fmt.Println(logger)
+}
+
+func f(counterMap LogCounterMap) error {
+	var status string
+	defer func() {
+		// here stutus reference a variable outside the closure body
+		// hence, status is evaluated when the closure is executed
+		// and not when call defer.
+		counterMap.incrementCounter(status)
+	}()
+
+	if err := foo(); err != nil {
+		status = StatusErrorFoo
+		return err
+	}
+	if err := bar(); err != nil {
+		status = StatusErrorBar
+		return err
+	}
+
+	status = StatusSuccess
+	return nil
+}
+
+func foo() error {
+	if 1 == rand.Intn(5) {
+		return errors.New("err in foo")
+	}
+	return nil
+}
+func bar() error {
+	if 2 == rand.Intn(5) {
+		return errors.New("err in bar")
+	}
+	return nil
+}
+
+type LogCounterMap map[string]int
+
+func (m LogCounterMap) incrementCounter(status string) {
+	m[status] += 1
 }
