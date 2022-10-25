e72e148671b543549076d0a7682b260fcb539f4f
OusManDiouf <java-89@hotmail.fr>
2022-10-25 09:46:48 +0000

    On going...

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Go!!!!!")
+	s := []string{"Cammyhaven", "East Willisstad", "Lake Thômas"}
+	stringConcatV1 := subOptimalStringConcat(s)
+	stringConcatV2 := stringConcat(s)
+	stringConcatV3 := optimalStringConcat(s)
+	fmt.Println(stringConcatV1)
+	fmt.Println(stringConcatV2)
+	fmt.Println(stringConcatV3)
+}
+
+func subOptimalStringConcat(str []string) string {
+	var s string
+	for _, v := range str {
+		s += v
+	}
+	return s
+}
+
+func stringConcat(values []string) string {
+	sb := strings.Builder{}
+	for _, v := range values {
+		sb.WriteString(v)
+	}
+	return sb.String()
+}
+
+func optimalStringConcat(values []string) string {
+	total := 0
+	// here we are not interested in the number of runes
+	// but the number of bytes hence we use the len function
+	for i := 0; i < len(values); i++ {
+		total += len(values[i])
+	}
+	sb := strings.Builder{}
+	// preallocate a total number bytes before iteration
+	sb.Grow(total)
+	for _, v := range values {
+		sb.WriteString(v)
+	}
+	return sb.String()
 }
