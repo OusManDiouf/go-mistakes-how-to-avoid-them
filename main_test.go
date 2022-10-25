d30b6134a038e2357b31c9877c7f230ab3f300a9
OusManDiouf <java-89@hotmail.fr>
2022-10-25 16:24:10 +0000

    Wrong way of doing

--- main_test.go
+++ main_test.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
-func TestSome(t *testing.T) {
+func TestCountEmptyLineInFileV1(t *testing.T) {
 
-	assert.True(t, true)
+	count, err := countEmptyLineInFileV1("file.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 5, count, "count not match")
+}
+
+func countEmptyLineInFileV1(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	count := 0
+	sc := bufio.NewScanner(file) //by default, split the input per line
+	for sc.Scan() {
+
+		if sc.Text() == "" {
+			//if len(sc.Text()) == 0 {
+			//if len(sc.Bytes()) == 0 {
+			//if utf8.RuneCountInString(sc.Text()) == 0 {
+			count += 1
+		}
+	}
+	return count, err
 }
