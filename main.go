c1788a1e3528849ac38b60910bfc9e78fa8887cf
OusManDiouf <java-89@hotmail.fr>
2022-10-29 18:36:38 +0000

    done(need review)

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type Position struct {
+	lat  int
+	long int
+}
+
+type publisher interface {
+	Publish(ctx context.Context, ch chan Position)
+}
+
+type radarPublisher struct{}
+
+func (rp radarPublisher) Publish(ctx context.Context, ch chan Position) {
+	for {
+		select {
+		case v := <-ch:
+			fmt.Printf("%v sent to kafka\n", v)
+			break
+		case <-ctx.Done():
+			fmt.Println("Timeout.")
+			return
+		}
+	}
+	//time.Sleep(10 * time.Second)
+	//fmt.Println("Position published to kafka...")
+}
+
+type publishHandler struct {
+	pub publisher
+}
+
+func (h publishHandler) publishPosition(p Position) error {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelFunc()
+
+	ch := make(chan Position)
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch <- p
+	}()
+	h.pub.Publish(ctx, ch)
+	return nil
+}
 
 func main() {
-	fmt.Println("Go!!!!!")
+	position := Position{
+		lat:  10,
+		long: 20,
+	}
+	radarPub := radarPublisher{}
+
+	h := publishHandler{pub: radarPub}
+	err := h.publishPosition(position)
+	if err != nil {
+		return
+	}
 }
