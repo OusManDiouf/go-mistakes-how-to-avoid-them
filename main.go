4b5354d2dcdea1eac454f169d474b5f43536680b
OusManDiouf <java-89@hotmail.fr>
2022-10-30 22:09:41 +0000

    done(but the signal is still blocking the main goRoutine...)

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Donation struct {
+	cond    *sync.Cond
+	balance int
+}
 
 func main() {
-	fmt.Println("Go!!!!!")
+
+	donation := Donation{
+		cond: sync.NewCond(&sync.Mutex{}),
+	}
+
+	// Listener goRoutines
+
+	f := func(goal int) {
+		donation.cond.L.Lock()
+		// if the goal is not met yet
+		// First Critical section
+		for donation.balance < goal {
+			// wait for a condition (balance update) within lock/unlock
+			// wait marche comme suit:
+			//	-> Unlock le mutex
+			//	-> Suspend la goRoutine courrante, et atteint une notification
+			// -> Lock le mutex lorqu'une notification arrive
+			donation.cond.Wait()
+		}
+		// Second Critical section
+		fmt.Printf("%d goal reached\n", donation.balance)
+		donation.cond.L.Unlock()
+	}
+
+	go f(10)
+	go f(15)
+
+	// Updater goRoutine
+	for {
+		time.Sleep(time.Second)
+		fmt.Println("Current Balance: ", donation.balance)
+		donation.cond.L.Lock()
+		// Critical section: locked to prevent data races
+		donation.balance++
+		donation.cond.L.Unlock()
+		// reveille tous les goRoutines qui attende sa condition (à savoir à chaque update)
+		donation.cond.Broadcast()
+	}
 }
