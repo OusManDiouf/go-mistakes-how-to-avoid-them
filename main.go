13d700b933a8fa4cd2b151eee0e904ce359915cf
OusManDiouf <java-89@hotmail.fr>
2022-10-30 11:12:39 +0000

    on going(look for a wai tho implement the client side)

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Go!!!!!")
+	//ch := make(chan int, 1) // buffered channel
+	//ch <- 1                 // non-blocking; buff not filed
+	//ch <- 2                 // blocking: buff already filed!
 }
