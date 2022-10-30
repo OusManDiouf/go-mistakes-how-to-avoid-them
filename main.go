74f20967756a95ea4ee6c0bde21e82a1efbf832c
OusManDiouf <java-89@hotmail.fr>
2022-10-30 04:12:42 +0000

    on going(look for a wai tho implement the client side)

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Go!!!!!")
+
+	// this channel is nil. the main goRoutine won't panic. It will just block FaEver !
+	//var ch chan int
+	//<-ch
+
+	// the same apply if we send through a nil chan. the main goRoutine won't panic. It will just block FaEver !
+	//var ch chan int
+	//ch <- 0
+
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+
+	_ = memoryHogChanMerge(ch1, ch2)
+
+}
+
+func memoryHogChanMerge(ch1, ch2 chan int) <-chan int {
+	ch := make(chan int, 1)
+	ch1Closed := false
+	ch2Closed := false
+
+	go func() {
+		for {
+			select {
+			case v, open := <-ch1:
+				if !open {
+					ch1Closed = true
+				}
+				ch <- v
+			case v, open := <-ch2:
+				if !open {
+					ch2Closed = true
+				}
+				ch <- v
+			}
+			if ch1Closed && ch2Closed {
+				close(ch)
+				return
+			}
+		}
+	}()
+	return ch
 }
