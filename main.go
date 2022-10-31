c0d15639cccef39f29e9a8f95013ea0884d8bf62
OusManDiouf <java-89@hotmail.fr>
2022-10-31 12:58:04 +0000

    done

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Event struct {
+	Time time.Time
+}
 
 func main() {
-	fmt.Println("Go!!!!!")
+
+	// Example d'utilisation de local
+	//voir dans /usr/local/go/lib/time/zoneinfo.zip pour les timezones dispo
+	location, err := time.LoadLocation("Europe/Paris")
+	if err != nil {
+		return
+	}
+	time1 := time.Now().In(location)
+	fmt.Println(time1)
+
+	event1 := Event{
+		Time: time.Now().UTC(),
+	}
+	bytes, err := json.Marshal(event1)
+	if err != nil {
+		return
+	}
+	var event2 Event
+	err = json.Unmarshal(bytes, &event2)
+	if err != nil {
+		return
+	}
+	fmt.Println(event1.Time.Equal(event2.Time))
+	fmt.Println(event1)
+	fmt.Println(event2)
+
 }
