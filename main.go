de9a2c3a92ea5d874741131033aec23d991518f3
OusManDiouf <java-89@hotmail.fr>
2022-10-31 15:44:19 +0000

    done

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Go!!!!!")
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := foo(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return // always return after an error in an http handler
+		}
+		_, _ = fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	}))
+
+	log.Fatalln(http.ListenAndServe("localhost:8080", nil))
+}
+
+func foo(r *http.Request) error {
+	return errors.New("an error occured")
 }
