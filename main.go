8e8b5a9c7862cc007f751686479a2aa54f521bc5
OusManDiouf <java-89@hotmail.fr>
2023-01-13 21:57:43 +0000

    done

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Go!!!!!")
+	src := []int{1, 2, 3}
+	//var dest []int
+	dest := make([]int, len(src))
+
+	// Copy returns the number of elements copied,
+	// which will be the minimum of len(src) and len(dst).
+	copy(dest, src)
+	fmt.Println("len(dest): ", len(dest))
+	fmt.Println("len(src): ", len(src))
+	fmt.Println("src: ", src)
+	fmt.Println("dest: ", dest)
 }
