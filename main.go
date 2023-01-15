81cef0b86fa8413df1a6f8c41cf18d897ac1462d
OusManDiouf <java-89@hotmail.fr>
2023-01-15 23:51:22 +0000

    done

--- main.go
+++ main.go
@@ -13,9 +13,19 @@ func main() {
 		{balance: 300.},
 	}
 
+	//for _, a := range accounts {
+	//	// the struct 'a' is a copy so its mutation is only local to the range scope!
+	//	a.balance += 1000
+	//}
+
 	for i := range accounts {
 		accounts[i].balance += 1000
 	}
+
+	// used when we only need to update some value
+	//for i := 0; i < len(accounts); i++ {
+	//	accounts[i].balance += 1000
+	//}
 
 	fmt.Println(accounts)
 
