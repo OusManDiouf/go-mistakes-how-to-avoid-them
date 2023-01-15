bdaf74195e72186eebc7751002fe136a5cd11243
OusManDiouf <java-89@hotmail.fr>
2023-01-15 07:18:10 +0000

    done

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+/**
+Just like with slices, if we know up front the number of elements a map will contain,
+we should create it by providing an initial size. Doing this avoids potential map growth,
+which is quite heavy computation-wise because it requires reallocating enough space and rebalancing all the elements.
+*/
 package main
 
 import "fmt"
