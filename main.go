fdfd10e3b4d177160cc61d5a1546e3c3f2d4d434
OusManDiouf <java-89@hotmail.fr>
2023-03-03 01:30:29 +0000

    [DONE]

--- main.go
+++ main.go
@@ -2,101 +2,183 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 	"sync/atomic"
 )
 
+// Taken from kennedy
+//func init() {
+//	// here we are running against 2P
+//	runtime.GOMAXPROCS(2)
+//	//rand.Seed(time.Now().UnixNano())
+//}
+
 func main() {
-	//for i := 0; i < 10; i++ {
-	//	nonDeterministicExampleOfAntiDataRace()
+
+	/**
+	This 3 operations will eventually do they job
+	regardless of the execution order of the there goroutines
+	There is no data-race
+	But we still have a race condition issue.
+	*/
+	//DataRaceSolutionAtomic()
+	//DataRaceSolutionMutex()
+	//DataRaceSolutionChannel()
+
+	//for i := 0; i < 100_000; i++ {
+	//	NonDeterministicCode()
 	//}
-	//antiDataRaceV3()
-	//antiDataRaceV2()
-	///antiDataRaceV1()
-	fmt.Println(runtime.GOOS)
-	fmt.Println(runtime.Compiler)
-	fmt.Println(runtime.GOARCH)
-	fmt.Println(runtime.Version())
+
+	//Solution : S'assurer que les deux goroutines se lance dans le bon ordre.
+	for i := 0; i < 100_000; i++ {
+		DeterministicCodeUsingChannel()
+	}
+
+	//fmt.Println(runtime.GOOS)
+	//fmt.Println(runtime.Compiler)
+	//fmt.Println(runtime.GOARCH)
+	//fmt.Println(runtime.Version())
 }
 
-func nonDeterministicExampleOfAntiDataRace() {
+// NonDeterministicCode
+//	but data race free !
+func NonDeterministicCode() {
+	// Il n'y a pas de data-race ici,
+	// mais on est bien en presence d'une race condition,
+	// du fait de l'aspect non-deterministic :
+	// le résultat depend de la sequence ou le timing des events
+	// qui ne peuvent pas être contrôlé.
+	// Here, the timing of events is the goroutines’ execution order.
+
 	i := 0
 	mu := sync.Mutex{}
 	var wg sync.WaitGroup
 	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		mu.Lock()
+		defer mu.Unlock()
+		i += 2
+	}()
 
 	func() {
-		mu.Lock()
 		defer wg.Done()
+
+		mu.Lock()
 		defer mu.Unlock()
-		i = 111
+		i *= 2_000_000
 	}()
 
-	go func() {
-		mu.Lock()
-		defer wg.Done()
-		defer mu.Unlock()
-		i = 222
-	}()
 	wg.Wait()
-	fmt.Println("i: ", i)
+	fmt.Println("I = ", i)
 
 }
 
-func antiDataRaceV3() {
+func DeterministicCodeUsingChannel() {
 	i := 0
+	ch := make(chan struct{})
+
+	go func() {
+		i += 2
+		<-ch
+	}()
+
+	ch <- struct{}{}
+
+	go func() {
+		i *= 2_000_000
+		<-ch
+	}()
+
+	ch <- struct{}{}
+	fmt.Println("I = ", i)
+}
+
+// DataRaceSolutionChannel (RECOMENDED)
+//		Share memory by communicating; don't communicate by sharing memory.
+//      -> Use communication and channels to ensure that
+//         a variable is updated by only one goroutine
+func DataRaceSolutionChannel() {
+	i := 0
+
+	// Unbuffered channel: G sender wait until the G receiver is ready
 	ch := make(chan int)
 	go func() {
-		ch <- 1
+		ch <- 1 // "send" happen here
 	}()
 	go func() {
-		ch <- 1
+		ch <- 1 // "send" happen here
 	}()
-	i += <-ch
-	i += <-ch
 
-	fmt.Println("i:", i)
+	// No need for a wg here, No data-race because,
+	// the above senders will block until their respective receiver bellow are ready
+	fmt.Println("I = ", i) // read happen
+
+	// La main goroutine est la seule à accéder en ecriture à cette variable
+	// cette fonction est donc free of data-race data-race !
+	// À noter aussi que les deux ecritures se deroule de maniere sequentiel
+	i += <-ch // G receiver (main routine) is ready here
+	i += <-ch // G receiver (main routine) is ready here
+
+	// No need for a wg here,No data-race,
+	// because the "write" already happen above
+	fmt.Println("I = ", i) // read happen
+
 }
 
-func antiDataRaceV2() {
+func DataRaceSolutionMutex() {
 	i := 0
 	mu := sync.Mutex{}
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	// Parallel exec des deux goroutines
+	// donc on a besoin d'un mécanisme
+	// pour syncroniser l'accès à la shared rss i.
 	go func() {
 		defer wg.Done()
 		mu.Lock()
-		i++
+		i++ // here is the critical section
 		mu.Unlock()
 	}()
 	go func() {
 		defer wg.Done()
 		mu.Lock()
-		i++
+		i++ // here is the critical section
 		mu.Unlock()
 	}()
 
 	wg.Wait()
-	fmt.Println(i)
+
+	// Cette instruction peut induire un data-race
+	// à cause du syscall qu'entraine Println
+	// c'est ce qui justifie l'usage du wg
+	fmt.Println("I = ", i)
 }
-func antiDataRaceV1() {
+func DataRaceSolutionAtomic() {
 	var i int64
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	// Parallel exec des deux goroutines
+	// donc on a besoin d'un mécanisme
+	// pour syncroniser l'accès à la shared rss i.
 	go func() {
-		wg.Done()
+		defer wg.Done()
 		atomic.AddInt64(&i, 1)
 	}()
 	go func() {
-		wg.Done()
+		defer wg.Done()
 		atomic.AddInt64(&i, 1)
 	}()
 
 	wg.Wait()
-	fmt.Println(i)
+	// Cette instruction peut induire un data-race
+	// à cause du syscall qu'entraine Println
+	// c'est ce qui justifie l'usage du wg
+	fmt.Println("I = ", i)
 }
--- mem_model.go
+++ mem_model.go
@@ -2,86 +2,128 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	guarantee5()
+
+	//------------------------ GO MEMORY MODEL ----------------------
+
+	GoMemModelGuarantee5()
 }
 
-func guarantee5() {
-	// unbuffered channel: La goroutine de l'émetteur bloque jusqu'à ce que la goroutine du récepteur soit prête.
-	// buffered channel  : La goroutine de l'émetteur ne se bloque que lorsque le tampon est plein.
+func GoMemModelGuarantee5() {
+	// Unbuffered channel :
+	//	La goroutine émeteur bloque jusqu'à ce que la goroutine du récepteur soit prête.
+	// Buffered channel :
+	//	La goroutine de l'émetteur ne se bloque que lorsque le tampon est plein.
 
-	// Cette dernière guarantie pourrait se montrer contre-intuitive.
-	// une reception à partir d'un channel unbuffered à lieu AVANT
-	// que l'envoie vers ce channel se complete!
+	// Cette guarantie pourrait se montrer contre-intuitive, car elle statue que :
+	// une reception à partir d'un unbuffered channel a lieu AVANT
+	// que l'envoie vers ce channel se complete !
 
-	// OPTION I: with buffered channel
-	// la goRoutine parent envoie un message pis lit la variable i,
-	// pendant que la goRoutine fille modifie la variable i et lit le message du parent
-	// ici on remarque qu'il y a data race car le read/write peut arrivé simultanement,
-	// la variable i n'est synchronisé.
-	// OPTION I: with unbuffered channel
-	// dans ce cas, le write est garantie de se produire avant le read.
-	// car la reception à partir d'un channel unbufferisé se produit AVANT
-	// l'envoie vers ce même channel, donc le write sur i se fera toujour
-	// avant le read de ce dernier dans la goRoutine parent.
-	// Cette ordre est garantie par le "Go Memory Model"
+	// OPTION I : With buffered channel
+	//	 La goRoutine parent envoie un message puis lit la variable i,
+	//	 pendant que la goRoutine fille modifie la variable i et lit le message du parent
+	//	 ici on remarque qu'il y a data race, car le read/write peut arriver simultanement,
+	//	 la variable i n'est synchronisé.
+	// OPTION II : With unbuffered channel
+	//	 Dans ce cas de figure, le write est garantie de se produire avant le read,
+	//	 parce que la reception à partir d'un channel unbufferisé ce produit AVANT
+	//	 l'envoie vers ce même channel, donc le write sur i se fera toujour
+	//	 avant le read de ce dernier dans la goRoutine parent.
+	//	 Cet ordre est garanti par le "Go Memory Model"
+
 	i := 0
-	//ch := make(chan struct{}, 1)
+	// first:  sender block until receive is ready
+	// second: reveive happen before send complete
 	ch := make(chan struct{})
+
+	//ch := make(chan struct{}, 1)
 	go func() {
+		// [with Unbuffered Channel] this "write"
+		// is guaranteed to happen before the read on the ch
 		i = 1
-		fmt.Println(<-ch)
+		fmt.Println(<-ch) // receive from ch
 	}()
+
+	// this will block with an unbuff chan
+	// but proceed with a buff chan (data race here)
 	ch <- struct{}{}
-	fmt.Println(i)
+	fmt.Println("[DONE] i = ", i)
 }
-func guarantee4() {
-	// la fermeture d'un channel à lieu avant reception de la notif de fermeture.
-	// le channel sera fermé une fois que le receiver receptionne la derniére value
-	// retourner via ce channel.
-	// ici, au lieu d'envoyer un message on ferme le channel
+func GoMemModelGuarantee4() {
+	// La fermeture d'un channel a lieu avant reception de la notif de fermeture.
+	// Le channel sera fermé une fois que le receiver receptionne
+	// la derniére valeur retournée par ce channel.
+	// Ici, au lieu d'envoyer un message, on ferme le channel
 	// Ce cas de figure est exempt de data race.
 	i := 0
+	isChClosed := func(t bool) string {
+		if t == false {
+			return "YES"
+		}
+		return "NO"
+	}
 	ch := make(chan struct{})
 	go func() {
 		_, ok := <-ch
-		fmt.Println("ok to go on ch: ", ok)
+		fmt.Println("is ch closed: ", isChClosed(ok))
 		fmt.Println("i: ", i)
 	}()
 	i++
+	// A call to a close(ch) should always happen on the sender,
+	// never on the receiver.
 	close(ch)
 }
-func guarantee3() {
-	// L'envoie vers un channel à lieu avant que le "receive from that channel" soit "complete"
-	// la goRoutine parent inc i avant l'envoie ver un channel, tandis qu'un autre goRoutine,
-	// lit la variable i juste aprés un channel read
+func GoMemModelGuarantee3() {
+	// SEND TO A CHANNEL HAPPEN BEFORE THE RECEIVE FROM THAT CHANNEL COPLETE.
+
+	// Dans l'exemple suivant, une G parent incrémente une variable avant un envoi,
+	// tandis qu'une autre G le lit après une lecture sur ce canal :
+
+	// la goRoutine parent inc i avant l'emission vers un channel,
+	// tandis qu'un autre goRoutine lit la variable i juste aprés un channel read
+
 	// orderd'exec: variable increment < channel send < channel receive < varible read
 	// par transitivité, on peut s'assurer que l'accés à i est synchronisé,
 	// du coup pas de data race
 	i := 0
-	ch := make(chan struct{})
-	go func() {
-		fmt.Println("ch res: ", <-ch)
-		fmt.Println("i: ", i)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	ch := make(chan struct{}) // Unbuff chan - G sender will block untill the G receiver is ready.
+	go func() {               //G1
+		fmt.Println("[PRINT FIRST]ch res: ", <-ch) // receive on ch channnel
+		fmt.Println("[PRINT SECOND]i: ", i)
+		defer wg.Done()
 	}()
 	i++
+	// here the main G will block until the above G receiver is ready
+	// Also the "send" will happen before any receive on ch channel
 	ch <- struct{}{}
+
+	// First : any write in G1 will force this to be synchronized via wg to avoid data race
+	// Second : l'affichage est random, ce qui veut dire que ce Print ne s'affiche pas en dernier
+	// tout le temps, pour le rendre deterministic, on doit introduire un wg
+	// pour avoir une guarantie d'afficher cette ligne tout le temps en dernier lieu
+	wg.Wait()
+	fmt.Println("------- [PRINT LAST] --------")
 }
-func guarantee2() {
-	// Cependant l'exit d'une goRoutine n'est pas guarantie de se dérouler avant un event.
-	// C'est pourquoi l'exemple suivant provoque un data race.
+func GoMemModelGuarantee2() {
+	// Cependant, la sortie d'une goRoutine n'est pas guarantie
+	// de se dérouler avant un qulquonque event.
+	// C'est pourquoi l'exemple suivant provoque un "data race".
 	// Si l'on veut empêcher cela, on doit synchroniser les goRoutines
 	i := 0
 	go func() {
 		i++
 	}()
-	fmt.Println("i: ", i)
+	fmt.Println("i: ", i) // syscall - data race - need sync via wg
 }
 
-func guarantee1() {
-	// la creation d'une goRoutine se passe avant le debut d'execution de la goRoutine.
+func GoMemModelGuarantee1() {
+	// Creating a goroutine happen before the goroutine exec begins.
+	// Data race free on this one; the read happen before the G execution.
 	// Donc, le fait de lire une variable et d'ensuite créer une goRoutine
 	// qui ecrase cette variable ne provoque pas de data race.
 	i := 0
